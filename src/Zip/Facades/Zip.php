5b61fc128bf0a8c1c93a5bb89ee51d7d388dec19
Bram Wubs <bwubs@me.com>
2015-04-20 11:44:24 +0200

    Added Facades folder

---
+++ src/Zip/Facades/Zip.php
@@ -0,0 +1,18 @@
+<?php
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Created by PhpStorm.
+ * User: bwubs
+ * Date: 20-04-15
+ * Time: 11:05
+ */
+class Zip extends Facade
+{
+
+    protected static function getFacadeAccessor()
+    {
+        return "zip";
+    }
+
+}
